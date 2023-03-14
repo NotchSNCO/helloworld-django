42d493a7bc5055e563cc0b7207124706841810b6
NOTCH <50923147+NotchSNCO@users.noreply.github.com>
2023-03-14 16:32:55 -0400

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello World! Django is running part 3!")
+    return HttpResponse("Hello World! Django is running part 4!")
